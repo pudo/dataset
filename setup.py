43c9905c1b61929d89d6297383f3504903cf1740
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-21 11:52:52 +0100

    basic python package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlaload',
+    version='0.1',
+    description="Utility functions for using SQLAlchemy in ETL.",
+    long_description='',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    keywords='sql sqlalchemy etl loading utility',
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    url='http://okfn.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        'sqlalchemy>=0.7',
+        'sqlalchemy-migrate>=0.7'
+    ],
+    tests_require=[],
+    entry_points=\
+    """ """,
+)
