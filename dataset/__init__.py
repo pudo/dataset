b76f11c94c5033c19c2ba49eada8992275e2b7c7
Gregor Aisch <contact@vis4.net>
2013-04-01 21:32:26 +0200

    added documentation

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,10 +1,20 @@
 # shut up useless SA warning:
 import warnings
-warnings.filterwarnings('ignore', 'Unicode type received non-unicode bind param value.')
+warnings.filterwarnings(
+    'ignore', 'Unicode type received non-unicode bind param value.')
 
 from dataset.persistence.database import Database
 from dataset.persistence.table import Table
 
-def connect(url):
-    return Database(url)
 
+def connect(url):
+    """ Opens a new connection to a database. *url* can be any valid `SQLAlchemy engine URL`_. Returns
+    an instance of :py:class:`dataset.Database.
+
+    .. code-block:: python
+
+        db = dataset.connect('sqlite:///factbook.db')
+
+    .. _SQLAlchemy Engine URL: http://docs.sqlalchemy.org/en/latest/core/engines.html#sqlalchemy.create_engine
+    """
+    return Database(url)
