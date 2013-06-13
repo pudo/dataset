57c37ac45331c173b72084b9e9c23dad7ea2afec
Friedrich Lindenberg <friedrich@pudo.org>
2013-06-13 12:02:56 +0200

    Up version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dataset',
-    version='0.3.4',
+    version='0.3.5',
     description="Toolkit for Python-based data processing.",
     long_description="",
     classifiers=[
