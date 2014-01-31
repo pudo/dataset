a34315818fd1f6d270b3226892e3b9005dbfbc16
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-01-31 21:07:39 +0100

    Fix bad python 2.6 detection

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 py26_dependency = []
-if sys.version_info <= (2, 6):
+if sys.version_info[:2] <= (2, 6):
     py26_dependency = ["argparse >= 1.2.1", "ordereddict >= 1.1"]
 
 setup(
