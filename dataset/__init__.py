d5a419cbe090e65d038a65830ceaae4304401d53
Simon Conseil <contact@saimon.org>
2015-02-18 14:45:45 +0100

    Reflect views.
    Currently views does not appear in the list of tables, this allows to add views.

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -12,14 +12,21 @@ from dataset.freeze.app import freeze
 __all__ = ['Database', 'Table', 'freeze', 'connect']
 
 
-def connect(url=None, schema=None, reflectMetadata=True, engine_kwargs=None):
+def connect(url=None, schema=None, reflectMetadata=True, engine_kwargs=None,
+            reflect_views=True):
     """
-    Opens a new connection to a database. *url* can be any valid `SQLAlchemy engine URL`_.
-    If *url* is not defined it will try to use *DATABASE_URL* from environment variable.
-    Returns an instance of :py:class:`Database <dataset.Database>`. Set *reflectMetadata* to False if you
-    don't want the entire database schema to be pre-loaded. This significantly speeds up
-    connecting to large databases with lots of tables. Additionally, *engine_kwargs* will be directly passed to
-    SQLAlchemy, e.g. set *engine_kwargs={'pool_recycle': 3600}* will avoid `DB connection timeout`_.
+    Opens a new connection to a database.
+
+    *url* can be any valid `SQLAlchemy engine URL`_.  If *url* is not defined
+    it will try to use *DATABASE_URL* from environment variable.  Returns an
+    instance of :py:class:`Database <dataset.Database>`. Set *reflectMetadata*
+    to False if you don't want the entire database schema to be pre-loaded.
+    This significantly speeds up connecting to large databases with lots of
+    tables. *reflect_views* can be set to False if you don't want views to be
+    loaded.  Additionally, *engine_kwargs* will be directly passed to
+    SQLAlchemy, e.g.  set *engine_kwargs={'pool_recycle': 3600}* will avoid `DB
+    connection timeout`_.
+
     ::
 
         db = dataset.connect('sqlite:///factbook.db')
@@ -34,4 +41,4 @@ def connect(url=None, schema=None, reflectMetadata=True, engine_kwargs=None):
         sqlite_datetime_fix()
 
     return Database(url, schema=schema, reflectMetadata=reflectMetadata,
-                    engine_kwargs=engine_kwargs)
+                    engine_kwargs=engine_kwargs, reflect_views=reflect_views)
