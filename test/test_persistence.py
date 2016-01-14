47917c686756a390fc6eebd300ec394aafd9f021
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-01-14 21:40:53 +0100

    Add failing test case for table drop

--- test/test_persistence.py
+++ test/test_persistence.py
@@ -304,6 +304,11 @@ class TableTestCase(unittest.TestCase):
         else:
             assert False, 'we should not reach else block, no exception raised!'
 
+    def test_table_drop(self):
+        assert 'weather' in self.db
+        self.db['weather'].drop()
+        assert 'weather' not in self.db
+
     def test_columns(self):
         cols = self.tbl.columns
         assert len(list(cols)) == 4, 'column count mismatch'
