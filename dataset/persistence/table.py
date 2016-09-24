5c04bd1e5d4af29d8dbd9b2fce22aad7dff7d5eb
Friedrich Lindenberg <friedrich@pudo.org>
2016-09-24 10:44:49 +0200

    allow for **kwargs in create_index, refs #172.

--- dataset/persistence/table.py
+++ dataset/persistence/table.py
@@ -333,7 +333,7 @@ class Table(object):
         finally:
             self.database._release()
 
-    def create_index(self, columns, name=None, unique=False):
+    def create_index(self, columns, name=None, **kw):
         """
         Create an index to speed up queries on a table.
 
@@ -361,7 +361,7 @@ class Table(object):
         try:
             self.database._acquire()
             columns = [self.table.c[c] for c in columns]
-            idx = Index(name, *columns, unique=unique)
+            idx = Index(name, *columns, **kw)
             idx.create(self.database.engine)
         except:
             idx = None
