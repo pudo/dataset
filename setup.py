fa1ec13f038d3a6df8e132eaec914a75bf03aa6c
Friedrich Lindenberg <friedrich@pudo.org>
2016-09-24 11:13:21 +0200

    up the version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info[:2] <= (2, 6):
 
 setup(
     name='dataset',
-    version='0.6.4',
+    version='0.7.0',
     description="Toolkit for Python-based data processing.",
     long_description="",
     classifiers=[
