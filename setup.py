4bb2338e13631daba110f4fe46700c59040866b0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-10-15 23:16:24 +0200

    Add Python 3.5, 3.6 to setup.py and travis

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
     ],
     keywords='sql sqlalchemy etl loading utility',
     author='Friedrich Lindenberg, Gregor Aisch, Stefan Wehrmeyer',
