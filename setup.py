fb4512783a81474fa5d5aa1641561ec337a8989e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-12-16 12:07:37 +0100

    Remove Python 2.6 classifier from setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
