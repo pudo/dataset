90e60857a2b50f37abd29994ab9d22c069cb2c84
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-03-09 11:38:50 +0100

    Add Python 3.7 to setup.py
    Closes #287

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='sql sqlalchemy etl loading utility',
     author='Friedrich Lindenberg, Gregor Aisch, Stefan Wehrmeyer',
