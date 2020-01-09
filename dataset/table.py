cbb74e6341a415987de84a93647744b772544229
James Salsman <jsalsman@gmail.com>
2020-01-09 13:20:01 -0800

    ilike
    in addition to like etc. in _args_to_clause()

--- dataset/table.py
+++ dataset/table.py
@@ -375,6 +375,8 @@ class Table(object):
                 key = list(value.keys())[0]
                 if key in ('like',):
                     clauses.append(self.table.c[column].like(value[key]))
+                elif key in ('ilike',):
+                    clauses.append(self.table.c[column].ilike(value[key]))
                 elif key in ('>', 'gt'):
                     clauses.append(self.table.c[column] > value[key])
                 elif key in ('<', 'lt'):
