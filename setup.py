c01e717436233c0012e5a10669e138420194898b
Friedrich Lindenberg <friedrich@pudo.org>
2020-02-23 19:17:20 +0100

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dataset',
-    version='1.2.0',
+    version='1.2.1',
     description="Toolkit for Python-based database access.",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -25,7 +25,7 @@ setup(
     include_package_data=False,
     zip_safe=False,
     install_requires=[
-        'sqlalchemy >= 1.2.0',
+        'sqlalchemy >= 1.2.1',
         'alembic >= 0.6.2'
     ],
     tests_require=[
